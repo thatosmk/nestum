112d4fbe9d4b5204c4506e9c49681e4cede7624a
Thato Semoko <thatosmk@gmail.com>
2018-06-19 11:44:25 +0200

    updated vimrc

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # setup up the config file
-mv vimrc ~/.vimrc
+cp vimrc ~/.vimrc
 
 # source the config file
 source ~/.vimrc
